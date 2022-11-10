9d3dfb3c0a789bf2798050d1ad368148d7940bdf
MacStudio <qin.batista@outlook.com>
2022-11-10 14:31:48 -0800

    -object moved by ECS -use ECS data

--- Assets/Scripts/MovingSystemBase.cs
+++ Assets/Scripts/MovingSystemBase.cs
@@ -5,9 +5,18 @@ public partial class MovingSystemBase : SystemBase
 {
     protected override void OnUpdate()
     {
-        foreach(TransformAspect transformAspect in SystemAPI.Query<TransformAspect>())
+        /*run in main thread*/
+        foreach((TransformAspect transformAspect, RefRO<ECSDataSpeed> _ECSDataSpeed) in SystemAPI.Query<TransformAspect,RefRO<ECSDataSpeed>>())
         {
-            transformAspect.Position += new float3(SystemAPI.Time.DeltaTime, 0, 0);
+            transformAspect.Position += new float3(SystemAPI.Time.DeltaTime*_ECSDataSpeed.ValueRO.value, 0, 0);
         }
+
+        /*run in different ways*/
+        // Entities.ForEach((TransformAspect TransformAspect)=>
+        // {
+        //     TransformAspect.Position += new float3(SystemAPI.Time.DeltaTime, 0, 0);
+        // }).ScheduleParallel(); //run multiple threads
+        // }).Schedule(); //run on single work thread
+        // }).Run(); //run on main thread
     }
 }
